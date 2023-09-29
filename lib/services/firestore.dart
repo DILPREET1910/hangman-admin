2a7d96dacb0ab5d80837d2f36309e63e3687ae27
DILPREET1910 <dsahney1910@gmail.com>
2023-09-29 13:25:02 +0530

    removed home page start admin access

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -4,23 +4,6 @@ import 'package:flutter/cupertino.dart';
 class Firestore {
   FirebaseFirestore firestoreInstance = FirebaseFirestore.instance;
 
-  Future<void> startGame() async {
-    Future<DocumentSnapshot> startGame =
-        firestoreInstance.collection("admin").doc("startGame").get();
-    bool start = false;
-
-    // get value
-    await startGame.asStream().forEach((element) {
-      start = element.get("start");
-    });
-
-    try {
-      await firestoreInstance.collection("admin").doc("startGame").update({"start": !start});
-    } on FirebaseException catch (error) {
-      print("Error while toggling game status: $error");
-    }
-  }
-
   Future<void> startQuestion(int questionNumber) async {
     DocumentSnapshot documentSnapshot =
         await firestoreInstance.collection("admin").doc("questions").get();
